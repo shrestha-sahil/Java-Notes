46a1ca9ac65d9bd91a49afb293c13e72b646e458
sahil52-git <sahilshrestha741@gmail.com>
2026-06-29 19:04:43 +0545

    ByteStreamExample

--- UnitFour/ByteStreamExample.java
+++ UnitFour/ByteStreamExample.java
@@ -16,7 +16,6 @@ public class ByteStreamExample {
             fos.write(contentBytes);
 
             System.out.println("File written successfully.");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
